b2cc3836e0fb9c300ae0cd0663f26edfd4f8c039
ali <alimiyan1112@gmail.com>
2024-02-21 11:46:25 +0530

    hash question added

--- Tree.js
+++ Tree.js
@@ -11,5 +11,49 @@ class Tree{
         this.root = null
     }
 
-    
+    isEmpty(){
+        return this.root == null
+    }
+
+    insert(data){
+        const nodeData = new Node(data)
+        if(this.isEmpty()){
+            this.root = nodeData
+        }else{
+            this.insertFunction(this.root,nodeData)
+        }
+    }
+
+    insertFunction(subRoot,node){
+        if(node.data < subRoot.data){
+            if(subRoot.left == null){
+                subRoot.left = node
+            }else{
+                this.insertFunction(subRoot.left,node)
+            }
+        }else{
+            if(subRoot.right == null){
+                subRoot.right = node
+            }else{
+                this.insertFunction(subRoot.right,node)
+            }
+        }
+    }
+
+
+ 
+    consoleValuesInOrder(node = this.root) {
+        if (node !== null) {
+            this.consoleValuesInOrder(node.left);
+            console.log(node.data);
+            this.consoleValuesInOrder(node.right);
+        }
+    }
 }
+
+const tree = new Tree()
+console.log(tree.isEmpty());
+tree.insert(10)
+tree.insert(20)
+tree.insert(3)
+tree.consoleValuesInOrder()
--- hashtable.js
+++ hashtable.js
@@ -148,3 +148,36 @@ var missingNumber = function(nums) {
 };  
 
 console.log(missingNumber([0,4,3,5,1]));//OUTPUT[2]  
+
+
+///////////////////////////////////LEET CODE 2956. Find Common Elements Between Two Arrays//////////////////////
+
+var findIntersectionValues = function(nums1, nums2) {
+  const map = new Map()
+  for(let i = 0; i < nums1.length; i++){
+      map.set(nums1[i],true)
+  }
+  let count = 0
+  for(let i = 0; i < nums2.length; i++){
+      if(map.has(nums2[i])){
+          count++
+      }
+  }
+  const map2 = new Map()
+  let count2 = 0
+  for(let i = 0; i < nums2.length; i++){
+      map2.set(nums2[i],true)
+  }
+  for(let i = 0; i < nums1.length; i++){
+      if(map2.has(nums1[i])){
+          count2++
+      }
+  }
+
+  return [count2,count]
+};
+
+console.log(findIntersectionValues([4,3,2,3,1],[2,2,5,2,3,6]));//Output: [3,4]
+// Explanation: We calculate the values as follows:
+// - The elements at indices 1, 2, and 3 in nums1 occur at least once in nums2. So the first value is 3.
+// - The elements at indices 0, 1, 3, and 4 in nums2 occur at least once in nums1. So the second value is 4.
