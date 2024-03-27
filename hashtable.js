a95922684e9be9c84859aa7e4cfeffd4da8e910c
ali <alimiyan1112@gmail.com>
2024-03-27 06:56:22 +0530

    leet code added

--- hashtable.js
+++ hashtable.js
@@ -277,3 +277,29 @@ var repeatedNTimes = function(nums) {
 };
 
 console.log(repeatedNTimes([1,2,3,3]));//Output: 3
+
+/////////////////////////////////////LEET CODE:2215. Find the Difference of Two Arrays/////////////////////////
+
+var findDifference = function(nums1, nums2) {
+  const s1 = new Set(nums1);
+  const s2 = new Set(nums2);
+  
+  let arr1 = [];
+  let arr2 = [];
+
+  for (let num of s1) {
+      if (!s2.has(num)) {
+          arr1.push(num);
+      }
+  }
+
+  for (let num of s2) {
+      if (!s1.has(num)) {
+          arr2.push(num);
+      }
+  }
+
+  return [arr1, arr2];
+};
+
+console.log(findDifference([1,2,3],[2,4,6]));//Output: [[1,3],[4,6]]
