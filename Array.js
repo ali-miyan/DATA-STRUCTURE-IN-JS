32a14505c026857d70531adf75410834ea5f66c1
ali <alimiyan1112@gmail.com>
2024-04-01 18:06:19 +0530

    array question added

--- Array.js
+++ Array.js
@@ -174,3 +174,21 @@ var sumOfEncryptedInt = function(nums) {
 
 
 console.log(sumOfEncryptedInt([10,21,31]));//Output: 66
+
+//////////////////////////////////////3069. Distribute Elements Into Two Arrays I///////////////////////////////
+
+var resultArray = function(nums) {
+
+    let arr1 = [nums[0]]
+    let arr2 = [nums[1]]
+
+    for(let i = 2; i < nums.length; i++) {
+        if(arr1[arr1.length-1] > arr2[arr2.length-1]){
+            arr1.push(nums[i])
+        }else{
+            arr2.push(nums[i])
+        }
+    }
+
+    return [...arr1,...arr2];
+};
